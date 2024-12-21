926dfb3a030c8bd6ea1b63017b685271c6750ef7
jhaasbroek <j.hattingh.haasbroek@student.tue.nl>
2024-12-21 19:52:17 +0100

    Update tokenization function

--- data_tokenization.py
+++ data_tokenization.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords, wordnet
+from nltk.corpus import wordnet
 from nltk.tag import pos_tag
 from nltk.stem import WordNetLemmatizer
-nltk.download(['punkt_tab', 'stopwords', 'averaged_perceptron_tagger_eng', 'wordnet'])
+from tqdm import tqdm
+nltk.download(['punkt', 'stopwords', 'averaged_perceptron_tagger', 'wordnet', 'omw-1.4'])
 
 df_birth_year = pd.read_csv('datasets/birth_year.csv', sep=',')
 df_political_leaning = pd.read_csv('datasets/political_leaning.csv', sep=',')
-
-stop_words = set(stopwords.words("english")) - {'no', 'not', 'nor'}  # remove important negations from original list
-
 lemmatizer = WordNetLemmatizer()
+tqdm.pandas()  # add progress bar
 
 def pos_tagger(t: str):
     """
@@ -33,26 +32,21 @@ def pos_tagger(t: str):
 def tokenization(text: str) -> list[str]:
     """
     Tokenize the sentence.
-    :param text: text string to tokenize.
-    :return:
+    :param text: text string to tokenize
+    :return: list of tokens.
     """
-    pos_text = pos_tag(word_tokenize(text))
+    pos_text = pos_tag(word_tokenize(text))  # get POS for every word
     pos_tokens = [lemmatizer.lemmatize(word, pos_tagger(tag)).lower() for (word, tag) in pos_text]
     return pos_tokens
 
-## example of output
-df_by_h = df_birth_year.head().copy()
-df_by_h['post'] = df_by_h['post'].apply(tokenization)
-print(df_by_h)
 
-## create new df and csv file of df_birth_year and df_political_leaning
+## create new df and save as parquet file
 # df_birth_year_tokenized = df_birth_year.copy()
-# df_birth_year_tokenized['post'] = df_birth_year_tokenized['post'].apply(tokenization)
-# df_birth_year_tokenized.to_csv('datasets/birth_year_tokenized.csv', index=False, header=True)
-# print("CSV done")  # debug
+# df_birth_year_tokenized['post'] = df_birth_year_tokenized['post'].progress_apply(tokenization)
+# df_birth_year_tokenized.to_parquet('datasets/birth_year_tokenized.parquet')
+# print("'birth_year_tokenized.parquet' tokenized and saved.")
 
 # df_political_leaning_tokenized = df_political_leaning.copy()
-# df_political_leaning_tokenized['post'] = df_political_leaning_tokenized['post'].apply(tokenization)
-# df_political_leaning_tokenized.to_csv('datasets/political_leaning_tokenized.csv')
-# print("CSV done")  # debug
-
+# df_political_leaning_tokenized['post'] = df_political_leaning_tokenized['post'].progress_apply(tokenization)
+# df_political_leaning_tokenized.to_parquet('datasets/political_leaning_tokenized.parquet')
+# print("'political_leaning_tokenized.parquet' tokenized and saved.")
